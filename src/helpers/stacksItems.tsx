e18f5d9d43d0d433c96be6d2a5a6ab55722f48ef
eduardojzv <ezamorava@est.utn.ac.cr>
2024-12-20 00:11:41 -0600

    my stack

--- src/helpers/stacksItems.tsx
+++ src/helpers/stacksItems.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { IconHTML, IconReact } from "../icons/technologies";
+import { IconBootsTrap, IconCss, IconFastApi, IconHTML, IconJs, IconMySql, IconNextJs, IconPostGreSql, IconPython, IconReact, IconSqlServer, IconTailwind } from "../icons/technologies";
 
 interface stackItems {
     [key: string]: {
@@ -12,27 +12,19 @@ export const techStack: stackItems = {
     frontend: [
         { logo: <IconReact />, item: "React" },
         { logo: <IconHTML />, item: "HTML" },
-        { logo: "/placeholder.svg?height=50&width=50", item: "CSS" },
-        { logo: "/placeholder.svg?height=50&width=50", item: "JavaScript" },
-        { logo: "/placeholder.svg?height=50&width=50", item: "Next.js" },
-        { logo: "/placeholder.svg?height=50&width=50", item: "Tailwind CSS" },
-        { logo: "/placeholder.svg?height=50&width=50", item: "Bootstrap" },
+        { logo: <IconCss />, item: "CSS" },
+        { logo: <IconJs />, item: "JavaScript" },
+        { logo: <IconNextJs />, item: "Next.js" },
+        { logo: <IconTailwind />, item: "Tailwind CSS" },
+        { logo: <IconBootsTrap />, item: "Bootstrap" },
     ],
     backend: [
-        { logo: "/placeholder.svg?height=50&width=50", item: "Python" },
-        { logo: "/placeholder.svg?height=50&width=50", item: "FastAPI" },
-        { logo: "/placeholder.svg?height=50&width=50", item: "Node.js" },
-        { logo: "/placeholder.svg?height=50&width=50", item: "Express" },
+        { logo: <IconPython />, item: "Python" },
+        { logo: <IconFastApi />, item: "FastAPI" },
     ],
     databases: [
-        { logo: "/placeholder.svg?height=50&width=50", item: "MySQL" },
-        { logo: "/placeholder.svg?height=50&width=50", item: "SQL Server" },
-        { logo: "/placeholder.svg?height=50&width=50", item: "MongoDB" },
-    ],
-    libraries: [
-        { logo: "/placeholder.svg?height=50&width=50", item: "Redux" },
-        { logo: "/placeholder.svg?height=50&width=50", item: "Axios" },
-        { logo: "/placeholder.svg?height=50&width=50", item: "Jest" },
-        { logo: "/placeholder.svg?height=50&width=50", item: "Lodash" },
+        { logo: <IconMySql />, item: "MySQL" },
+        { logo: <IconSqlServer />, item: "SQL Server" },
+        { logo: <IconPostGreSql />, item: "PostGreSql" },
     ],
 };
--- src/sections/myStack/myStack.tsx
+++ src/sections/myStack/myStack.tsx
@@ -1,20 +1,19 @@
 import { techStack } from "../../helpers/stacksItems";
 import styles from "./myStack.module.css";
 
-const { stack__container, category__container, tech__item, tech__logo, tech__name } = styles;
+const { stack__container, category__container, tech__item, tech__logo,category__items,category__title} = styles;
 
 export default function MyStack() {
     return (
         <section className={stack__container}>
-            <h1>En proceso</h1>
             {Object.entries(techStack).map(([category, items]) => (
                 <div key={category} className={category__container}>
-                    <h2>{category.toUpperCase()}</h2>
-                    <div>
+                    <h2 className={category__title}>{category.toUpperCase()}</h2>
+                    <div className={category__items}>
                         {items.map(({ logo, item }) => (
                             <div key={item} className={tech__item}>
                                 <div className={tech__logo}>{logo}</div>
-                                <span className={tech__name}>{item}</span>
+                                <span>{item}</span>
                             </div>
                         ))}
                     </div>
